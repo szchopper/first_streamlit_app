bc86d959bc0cee735666d5cbf9549a818b9bdf60
szchopper <136897216+szchopper@users.noreply.github.com>
2023-06-17 13:42:23 +0200

    Update streamlit_app.py
    typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my fruit list = my fruit list.set index('Fruit")
+my_fruit_list = my fruit list.set index('Fruit")
 
 streamlit.title('My Parents New Healthy Diner')
 
