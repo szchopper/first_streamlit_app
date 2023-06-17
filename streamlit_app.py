c800c3205ec096a08a8864ebed6271ab289cb52c
szchopper <136897216+szchopper@users.noreply.github.com>
2023-06-17 18:20:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # print datatable
 streamlit.dataframe(fruityvice_normalized)
 
+
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
